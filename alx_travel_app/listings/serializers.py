a41541a67741c8ceabd3adbb2bf66201e8d44cb3
sossesam <sossesam@gmail.com>
2025-09-21 23:25:22 +0100

    first commit

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,18 @@
-pass
+from .models import Listing, Booking
+from rest_framework import serializers
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    """Serializer for the Listing model"""
+    
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    """Serializer for the Booking model"""
+    
+    class Meta:
+        model = Booking
+        fields = '__all__'
